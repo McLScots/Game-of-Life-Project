bbb5f7b704ba6e4f73b7fef054a4e39be4f7e66b
McLScots <78045527+McLScots@users.noreply.github.com>
2021-03-04 12:45:12 -0600

    Add files via upload

---
+++ MVC/Main.java
@@ -0,0 +1,11 @@
+//(C) Joseph Mack 2011, jmack (at) wm7d (dot) net, released under GPL v3 (or any later version)
+
+public class Main{
+
+	public static void main(String[] args){
+
+		RunMVC mainRunMVC = new RunMVC();
+
+	} //main()
+
+} //Main
